a44c7f2f68129aae0e6035ed3e8053d7ad4c4730
Robert O'Rourke <rob@humanmade.com>
2019-03-27 19:53:11 +0700

    Update load.php
    Co-Authored-By: joehoyle <joe@humanmade.co.uk>

--- load.php
+++ load.php
@@ -8,5 +8,5 @@ add_action( 'hm-platform.modules.init', function () {
 	register_module( 'search', __DIR__, 'Search', [ 'enabled' => true ], function () {
 		add_action( 'plugins_loaded', __NAMESPACE__ . '\\bootstrap' );
 		add_filter( 'hm_platform_healthchecks', __NAMESPACE__ . '\\add_elasticsearch_healthcheck' );
-	});
+	} );
 } );
