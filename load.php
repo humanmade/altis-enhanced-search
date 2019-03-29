657380b21b5b5c506f8c3e5cd32edef684441da4
Joe Hoyle <joehoyle@gmail.com>
2019-03-29 15:30:57 +0700

    Add option for index-documents

--- load.php
+++ load.php
@@ -5,7 +5,11 @@ namespace HM\Platform\Enhanced_Search;
 use function HM\Platform\register_module;
 
 add_action( 'hm-platform.modules.init', function () {
-	register_module( 'search', __DIR__, 'Search', [ 'enabled' => true ], function () {
+	$default_settings = [
+		'enabled' => true,
+		'index-documents' => true,
+	];
+	register_module( 'search', __DIR__, 'Search', $default_settings, function () {
 		add_action( 'plugins_loaded', __NAMESPACE__ . '\\bootstrap' );
 		add_filter( 'hm_platform_healthchecks', __NAMESPACE__ . '\\add_elasticsearch_healthcheck' );
 	} );
