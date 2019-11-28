f28a355fbf78cc41e0db22bb8952c3696bd2caf1
Robert O'Rourke <robert@o-rourke.org>
2019-11-28 11:59:41 +0000

    wording change

--- inc/namespace.php
+++ inc/namespace.php
@@ -77,7 +77,7 @@ function load_elasticpress() {
 	}
 
 	if ( defined( 'WP_CLI' ) && WP_CLI ) {
-		// Raise error reporting level for the index command as it will generate
+		// Raise error reporting threshold for the index command as it will generate
 		// a benign warning when the index doesn't already exist.
 		WP_CLI::add_hook( 'before_invoke:elasticpress index', function () {
 			error_reporting( E_ERROR );
