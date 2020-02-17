d6ab569c7c513007611d2aed17d2dd6dfbafd363
Robert O'Rourke <robert@o-rourke.org>
2020-02-17 12:34:16 +0000

    split out register callback before function guard

--- load.php
+++ load.php
@@ -5,16 +5,18 @@ namespace Altis\Enhanced_Search;
 
 use function Altis\register_module;
 
-// Don't self-initialize if this is not an Altis execution.
-if ( ! function_exists( 'add_action' ) ) {
-	return;
-}
-
-add_action( 'altis.modules.init', function () {
+function register() {
 	$default_settings = [
 		'enabled' => true,
 		'index-documents' => true,
 		'slowlog_thresholds' => true,
 	];
 	register_module( 'search', __DIR__, 'Search', $default_settings, __NAMESPACE__ . '\\bootstrap' );
-} );
+}
+
+// Don't self-initialize if this is not an Altis execution.
+if ( ! function_exists( 'add_action' ) ) {
+	return;
+}
+
+add_action( 'altis.modules.init', __NAMESPACE__ . '\\register' );
