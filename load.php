0224bbffa714b4211b4d916a20855056b9102e1b
Sean McCafferty <sean@mccafferty.co>
2020-04-08 23:00:20 -0400

    Add default for `protected-content`

--- load.php
+++ load.php
@@ -14,6 +14,7 @@ add_action( 'altis.modules.init', function () {
 		'woocommerce'        => false,
 		'autosuggest'        => false,
 		'slowlog_thresholds' => true,
+		'protected-content'  => true,
 	];
 	register_module( 'search', __DIR__, 'Search', $default_settings, __NAMESPACE__ . '\\bootstrap' );
 } );
